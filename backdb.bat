6102bdd8460b0b64afdea6ffbfd3313ee4222d4c
Abdelouahed Aliali <aaliali@e1r7p2.1337.ma>
2026-06-25 17:06:13 +0100

    reer

---
+++ backdb.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Database settings
+set DATABASE_NAME=mydb
+set USER=postgres
+set BACKUP_DIR=C:\backups
+set EXTERNAL_SSD_DRIVE=D:
+set EXTERNAL2=F:
+set EXTERNAL3=G:
+
+:: Get timestamp (YYYYMMDD_HHMMSS)
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set ldt=%%I
+set TIMESTAMP=!ldt:~0,8!_!ldt:~8,6!
+
+:: Backup file with timestamp
+set BACKUP_FILE=%BACKUP_DIR%\backup.sql
+
+echo Backing up database %DATABASE_NAME%...
+
+:: Set PGPASSFILE environment variable to point to .pgpass file
+set "PGPASSFILE=C:\Users\Public\pgpass.conf"
+
+"C:\Program Files\PostgreSQL\16\bin\pg_dump.exe" -h localhost -U %USER% -b -v -f "%BACKUP_FILE%" %DATABASE_NAME%
+
+echo Backup completed.
+
+echo Sending backup to remote server...
+
+scp "%BACKUP_FILE%" ilhamcasa@167.71.77.64:/home/ilhamcasa/ilhamcasabacakups
+
+echo Remote copy completed.
+
+endlocal
